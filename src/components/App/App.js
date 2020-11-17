2265b7f89f51d14b6c564bf2681abdd677471990
Dee <71417512+DeezDiamond@users.noreply.github.com>
2020-11-17 10:08:45 -0700

    Some updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Header from '../Header/Header';
+import GalleryDetails from "../GalleryDetails/GalleryDetails"
 
 const App = () => {
     return (
-        <div>
-            
+        <div className="App">
+            <Header />
+            <main>
+                <Route path='/' exact component={GalleryDetails} /> 
+
+                <Route
+                    path="gallery/:id"
+                    render={(routerProps) => {
+                        return <GalleryDetails match={routerProps.match} />
+                    }}
+                />
+            </main>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            
+            <h1>Time Capsule</h1>
+            <nav>
+                <Link to='/'>Home Page</Link>
+            </nav>
         </div>
     );
 };
