18288d29be870f3af14e997ab427a80b390d3966
shanestarkweather <shane@shanestarkweather.com>
2020-11-18 11:31:40 -0800

    Added state to submit form

--- src/components/Submit/Submit.js
+++ src/components/Submit/Submit.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Submit = () => {
+	const [gallery, setGallery] = useState({
+		title: '',
+		imgUrl: '',
+		caption: '',
+		eraTime: '',
+	});
 	return (
 		<div>
 			<form>
 				<label for='title'>Title:</label>
-				<input title='title' id='title' value={null} placeholder='Title' />
+				<input
+					onChange={handleChange}
+					title='title'
+					id='title'
+					value={gallery.title}
+					placeholder='Title'
+				/>
 				<label for='imgUrl'>Image URL:</label>
 				<input
+					onChange={handleChange}
 					title='imgUrl'
 					id='imgUrl'
-					value={null}
+					value={gallery.imgUrl}
 					placeholder='Image URL'
 				/>
 				<label for='caption'>Caption:</label>
 				<input
+					onChange={handleChange}
 					title='caption'
 					id='caption'
-					value={null}
+					value={gallery.caption}
 					placeholder='Caption'
 				/>
 				<label for='eraTime'>Image URL:</label>
 				<input
+					onChange={handleChange}
 					title='eraTime'
 					id='eraTime'
-					value={null}
+					value={gallery.eraTime}
 					placeholder='Era/Time'
 				/>
+				<button id='button' type='submit'>
+					Submit
+				</button>
 			</form>
 		</div>
 	);
