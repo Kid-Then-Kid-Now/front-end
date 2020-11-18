099e731cf7efa293584e6487620e0851911ab6a5
shanestarkweather <shane@shanestarkweather.com>
2020-11-17 17:25:37 -0800

    Working on connecting front to back

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import GalleryTitles from '../GalleryTitles/GalleryTitles';
 import Submit from '../Submit/Submit';
+import GalleryDetail from '../GalleryDetail/GalleryDetail';
 
 const App = () => {
 	return (
@@ -11,14 +12,12 @@ const App = () => {
 			<main>
 				<Switch>
 					<Route path='/' exact component={GalleryTitles} />
-
 					<Route
 						path='gallery/:id'
 						render={(routerProps) => {
-							return <GalleryTitles match={routerProps.match} />;
+							return <GalleryDetail match={routerProps.match} />;
 						}}
 					/>
-
 					<Route path='/submit' exact component={Submit} />
 				</Switch>
 			</main>
